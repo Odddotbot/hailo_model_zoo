8e145d1ae778da8b58091859871675c0850e04ad
ThomasObot <thomas@odd.bot>
2025-08-13 18:18:10 +0300

    Added part that evaluates parsed HAR

--- evaluation/evaluate.py
+++ evaluation/evaluate.py
@@ -8,17 +8,19 @@ import ultralytics
 
 
 # VARIABLES
-val_dir = Path('/datasets/hailo_calibration/val/images_448')
+val_dir = Path('/datasets/hailo_calibration/val/images')
 model_dir = Path('/experiments/ca_2025_06_26/train/ca_20250626_all_XL/weights')
-model_name = 'opt3'
+model_name = 'opt1'
 results_dir = Path('results')
 
 
 # CONSTANTS
 pt_filepath = model_dir / 'best.pt'
 onnx_filepath = model_dir / 'best.onnx'
-imgs_list = [cv2.imread(val_dir / img_path) for img_path in sorted(os.listdir(val_dir)) if img_path.endswith('jpg')]
+imgs_list = [cv2.resize(cv2.imread(val_dir / img_path), (448, 448)) for img_path in sorted(os.listdir(val_dir)) if img_path.endswith('jpg')]
 imgs_array = np.stack(imgs_list)
+output_layers = ['/model.22/cv2.0/cv2.0.2/Conv', '/model.22/cv3.0/cv3.0.2/Conv', '/model.22/cv2.1/cv2.1.2/Conv', '/model.22/cv3.1/cv3.1.2/Conv', '/model.22/cv2.2/cv2.2.2/Conv', '/model.22/cv3.2/cv3.2.2/Conv']
+layer_outputs = {}
 
 
 # HELPER FUNCTIONS
@@ -36,37 +38,61 @@ def process_raw_hailo_results(raw_results):
         results.append([weed_result, crop_result])
     return results
 
+def get_layer_output(name):
+    def hook(model, input, output):
+        output = output.detach().cpu().numpy()
+        b, c, w, h = output.shape
+        output = output.reshape((b, w, h, c))
+        layer_outputs[name] = output
+    return hook
+
 
 # INFERENCE & SAVING
 # Evaluate PyTorch
 model = ultralytics.models.yolo.model.YOLO(pt_filepath).to('cpu')
+# model.model.model[22].cv2[0][2].register_forward_hook(get_layer_output('/model.22/cv2.0/cv2.0.2/Conv'))
+# model.model.model[22].cv3[0][2].register_forward_hook(get_layer_output('/model.22/cv3.0/cv3.0.2/Conv'))
+# model.model.model[22].cv2[1][2].register_forward_hook(get_layer_output('/model.22/cv2.1/cv2.1.2/Conv'))
+# model.model.model[22].cv3[1][2].register_forward_hook(get_layer_output('/model.22/cv3.1/cv3.1.2/Conv'))
+# model.model.model[22].cv2[2][2].register_forward_hook(get_layer_output('/model.22/cv2.2/cv2.2.2/Conv'))
+# model.model.model[22].cv3[2][2].register_forward_hook(get_layer_output('/model.22/cv3.2/cv3.2.2/Conv'))
 results = [result.boxes.data for result in model.predict(imgs_list)]
-pickle_and_save_results('predictions.pt.pk', results)
+pickle_and_save_results(f'{model_name}.pt.pk', results)
 
 # Evaluate ONNX
 model = ultralytics.models.yolo.model.YOLO(onnx_filepath, task='detect')
 results = [model.predict(img)[0].boxes.data for img in imgs_list]
-pickle_and_save_results('predictions.onnx.pk', results)
+pickle_and_save_results(f'{model_name}.onnx.pk', results)
+
+# Evaluate parsed HAR
+# runner = ClientRunner(har=f'{model_dir.__str__()}/opt100_basic_parsing.har')
+# with runner.infer_context(InferenceContext.SDK_NATIVE) as ctx:
+#     raw_results = runner.infer(ctx, imgs_array)
+# for hailo_result, output_layer in zip(raw_results, output_layers):
+#     ultra_result = layer_outputs[output_layer]
+#     print(ultra_result.mean(), ultra_result.std())
+#     print(hailo_result.mean(), hailo_result.std())
+#     print(f"MAE({output_layer}):", np.abs(hailo_result - ultra_result).mean())
 
 # Evaluate Un-optimized HAR
 runner = ClientRunner(har=f'{model_dir.__str__()}/nothing.har')
 with runner.infer_context(InferenceContext.SDK_FP_OPTIMIZED) as ctx:
     raw_results = runner.infer(ctx, imgs_array)
 results = process_raw_hailo_results(raw_results)
-pickle_and_save_results('predictions.unoptimized_har.pk', results)
+pickle_and_save_results(f'{model_name}.unoptimized_har.pk', results)
 
 # Evaluate FP-optimized HAR
 runner = ClientRunner(har=f'{model_dir.__str__()}/{model_name}.har')
 with runner.infer_context(InferenceContext.SDK_FP_OPTIMIZED) as ctx:
     raw_results = runner.infer(ctx, imgs_array)
 results = process_raw_hailo_results(raw_results)
-pickle_and_save_results('predictions.fp_optimized_har.pk', results)
+pickle_and_save_results(f'{model_name}.fp_optimized_har.pk', results)
 
 # Evaluate Quantized HAR
 runner = ClientRunner(har=f'{model_dir.__str__()}/{model_name}.har')
 with runner.infer_context(InferenceContext.SDK_QUANTIZED) as ctx:
     raw_results = runner.infer(ctx, imgs_array)
 results = process_raw_hailo_results(raw_results)
-pickle_and_save_results('predictions.quantized_har.pk', results)
+pickle_and_save_results(f'{model_name}.quantized_har.pk', results)
 
 # TODO: Evaluate Compiled HAR on Raspberry Pi (or elsewhere I guess)
